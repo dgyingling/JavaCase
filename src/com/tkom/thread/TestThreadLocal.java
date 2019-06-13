ad8f0760d3356351631ff0a31602a186116e9b23
tkom <1539261793@qq.com>
2019-06-13 22:38:34 +0800

    ThreadLocal

---
+++ src/com/tkom/thread/TestThreadLocal.java
@@ -0,0 +1,32 @@
+package com.tkom.thread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by tkom on 2019/6/13 0013.
+ */
+public class TestThreadLocal {
+    private static ThreadLocal<String> threadLocal = new ThreadLocal<>();
+    public static void main(String[] args) {
+        ExecutorService threadService = Executors.newFixedThreadPool(5);
+        threadService.execute(()->{
+            threadLocal.set("1234");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName()+"----"+threadLocal.get());
+        });
+        threadService.execute(()->{
+            threadLocal.set("2222");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName()+"----"+threadLocal.get());
+        });
+    }
+}
